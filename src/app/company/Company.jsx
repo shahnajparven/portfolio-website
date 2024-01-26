51559777af8a0e14d40cafe7890486a385611cff
shahnaj parven <shahnajritu21@gmail.com>
2024-01-26 21:58:00 +0600

    change

--- src/app/company/Company.jsx
+++ src/app/company/Company.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Company = () => {
   return (
-    <div>Company</div>
-  )
-}
+    <div>
+      Company Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos
+      laboriosam omnis, dicta culpa, doloremque iure illum aperiam nemo, quas
+      aut deserunt itaque excepturi earum impedit magni nihil. Et, ab hic!s
+    </div>
+  );
+};
 
-export default Company
+export default Company;
